a7edc503a634dc5cb7cf0f5ef54984b4faac4cf5
James Male <james@james-male.co.uk>
2015-01-30 10:38:02 +0000

    Made it possible to select the type of rules to generate

--- index.py
+++ index.py
@@ -44,32 +44,30 @@ def generate_mimeheader():
 specify_ammount = input('How many rules would you like? ')
 print()
 
-# print('please select the type of rule you want using the below prompts')
-# print('press 1 for raw body rules')
-# print('press 2 for header rules')
-# print('press 3 for uri rules')
-# print('press 4 for mime header rules')
-# print()
-# specify_type = input(' > ')
+print('please select the type of rule you want using the below prompts')
+print('type \'raw\' for raw body rules')
+print('press \'header\' for header rules')
+print('press \'uri\' for uri rules')
+print('press \'mime\' mime header rules')
+print()
+specify_type = input(' > ')
 
-for _ in range(int(specify_ammount)):
+
+if specify_type == 'raw':
+    for _ in range(int(specify_ammount)):
         generate_rawbody()
 
-# if specify_type == 1:
-#     for _ in range(int(specify_ammount)):
-#         generate_rawbody()
-#
-#
-# if specify_type == 2:
-#     for _ in range(int(specify_ammount)):
-#         generate_header()
-#
-#
-# if specify_type == 3:
-#     for _ in range(int(specify_ammount)):
-#         generate_uri()
-#
-#
-# if specify_type == 4:
-#     for _ in range(int(specify_ammount)):
-#         generate_mimeheader()
+
+if specify_type == 'header':
+    for _ in range(int(specify_ammount)):
+        generate_header()
+
+
+if specify_type == 'uri':
+    for _ in range(int(specify_ammount)):
+        generate_uri()
+
+
+if specify_type == 'mime':
+    for _ in range(int(specify_ammount)):
+        generate_mimeheader()
